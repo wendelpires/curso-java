2366d9917c0cd59580b661f251d91100447fa718
Wendel Pires da Costa <wendel.pires94@gmail.com>
2024-06-15 14:57:51 -0300

    CursoJava

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+    /*Main é um método autoexecutavel em java*/
+	public static void main(String[] args) {
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em JAVA - Curso Formação JAva WEB");
+		System.out.println("Java é orientado a objetos");
+		
+	}
+}
